c4cd2277e9f2ea98d98e80bef63a1c8358287780
VictoryPashkova <victory.pashkova@gmail.com>
2023-12-26 16:30:37 +0600

    add network text

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,6 +3,10 @@ const ru = {
     errorInvalidUrl: 'Ссылка должна быть валидным URL',
     errorDoubUrl: 'RSS уже существует',
     successLoadedRSS: 'RSS успешно загружен',
+    postTitle: 'Посты',
+    feedTitle: 'Фиды',
+    viewButtonText: 'Просмотр',
+    networkErr: 'Network Error: invalid RSS',
   },
 };
 export default ru;
