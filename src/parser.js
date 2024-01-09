b5e3f2d5ea52d95ffc0a9c84a4d31188adc08f79
VictoryPashkova <victory.pashkova@gmail.com>
2024-01-09 17:26:10 +0600

    add getPosts&getFeeds fn

--- src/parser.js
+++ src/parser.js
@@ -1,18 +1,36 @@
-const getParsedData = (responseData) => {
+const getPosts = (postsData) => {
+  const postsInfo = [];
+  postsData.forEach((item) => {
+    const postName = item.querySelector('title').textContent;
+    const postLink = item.querySelector('link').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    postsInfo.push([postLink, postName, postDescription]);
+  });
+  return postsInfo;
+};
+
+const getFeeds = (titelFeedText, descriptionFeedText, feedLink) => ({
+  title: titelFeedText,
+  description: descriptionFeedText,
+  feedLink,
+});
+
+const getParsedData = (responseData, feedLink) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(responseData, 'application/xml');
 
   const errorNode = doc.querySelector('parsererror');
   if (errorNode) {
-    throw new Error('Ошибка парсинга');
+    throw new Error(errorNode);
   }
   const channel = doc.querySelector('channel');
-
   const titelFeedText = channel.querySelector('title').innerHTML;
   const descriptionFeedText = channel.querySelector('description').innerHTML;
-  const posts = channel.querySelectorAll('item');
+  const postsData = channel.querySelectorAll('item');
+  const feed = getFeeds(titelFeedText, descriptionFeedText, feedLink);
+  const posts = getPosts(postsData);
 
-  return { titelFeedText, descriptionFeedText, posts: { posts } };
+  return { feed, posts: { posts } };
 };
 
 export default getParsedData;
